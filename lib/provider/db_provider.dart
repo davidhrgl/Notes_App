78e01a2b2a8d3d91f517bbf3c7a7e4a4f192ea07
davidhrgl <davidhrgl@gmail.com>
2021-05-18 15:33:00 -0500

    Insert,Update and Delete en provider

--- lib/provider/db_provider.dart
+++ lib/provider/db_provider.dart
@@ -35,8 +35,6 @@ class DBProvider{
         CREATE TABLE note(
           id INTEGER PRIMARY KEY,
           title TEXT,
-          image TEXT,
-          tasks TEXT,
           content TEXT,
           color TEXT
         );
@@ -59,18 +57,90 @@ class DBProvider{
     );
   }
 
+
+  //===============> Functions to Note-TABLE <===========
   Future<int> newNote(NoteModel note) async{
     //Verify DB
     final db = await database;
     final int res = await db.insert("note", note.toJson());
     return res;
   }
+
+  Future<NoteModel> getNoteById(int id) async{
+    final db = await database;
+    final res = await db.query('note',where: 'id = ?',whereArgs: [id]);
+    return res.isNotEmpty 
+            ? NoteModel.fromJson(res.first)
+            : null;
+  }
+
+  Future<List<NoteModel>> getAllNotes() async{
+    final db = await database;
+    final res = await db.query('note');
+    return res.isNotEmpty 
+          ? res.map((e) => NoteModel.fromJson(e)).toList() 
+          : [];
+  }
+
+  Future<List<NoteModel>> getNotesByTitle(String title) async{
+    final db = await database;
+    final res = await db.query("note",where: "title like '%?%' ",whereArgs: [title]);
+    return res.isNotEmpty 
+          ? res.map((e) => NoteModel.fromJson(e)).toList() 
+          : [];
+  }
+
+  Future<int> updateNote(NoteModel note) async{
+    final db = await database;
+    final res = await db.update('note', note.toJson(),where: "id = ?",whereArgs: [note.id]);
+    return res;
+  }
+
+  Future<int> deleteNote(NoteModel note)async{
+    final db = await database;
+    this.deleteMediaByIdNote(note.id);
+    this.deleteTaskByIdNote(note.id);
+    final res = await db.delete('note',where: "id = ? ",whereArgs: [note.id]);
+    return res;
+  }
+  //===============> Functions to Media-TABLE <==================
+
   Future<int> newMedia(MediaModel media) async{
     //Verify DB
     final db = await database;
     final int res = await db.insert("media", media.toJson());
     return res;
   }
+
+  Future<MediaModel> getMediaById(int id) async{
+    final db = await database;
+    final res = await db.query('media',where: 'id = ?',whereArgs: [id]);
+    return res.isNotEmpty 
+            ? MediaModel.fromJson(res.first)
+            : [];
+  }
+
+  Future<List<MediaModel>> getAllMediaByIdNote(int idNote) async{
+    final db = await database;
+    final res = await db.query("media",where: "id_note = ? ",whereArgs: [idNote]);
+    return res.isNotEmpty 
+          ? res.map((e) => MediaModel.fromJson(e)).toList() 
+          : [];
+  }
+
+  Future<int> updateMedia(MediaModel media) async{
+    final db = await database;
+    final res = await db.update('media', media.toJson(),where: "id = ?",whereArgs: [media.id]);
+    return res;
+  }
+
+  Future<int> deleteMediaByIdNote(int idNote)async{
+    final db = await database;
+    final res = await db.delete('media',where: "id_note = ? ",whereArgs: [idNote]);
+    return res;
+  }
+  //===============> Functions to Task-TABLE <=====================
+
   Future<int> newTask(TaskModel task) async{
     //Verify DB
     final db = await database;
@@ -78,4 +148,31 @@ class DBProvider{
     return res;
   }
 
+  Future<TaskModel> getTaskById(int id) async{
+    final db = await database;
+    final res = await db.query('task',where: 'id = ?',whereArgs: [id]);
+    return res.isNotEmpty 
+            ? TaskModel.fromJson(res.first)
+            : [];
+  }
+
+  Future<List<TaskModel>> getAllTaskByIdNote(int idNote) async{
+    final db = await database;
+    final res = await db.query("task",where: "id_note = ? ",whereArgs: [idNote]);
+    return res.isNotEmpty 
+          ? res.map((e) => TaskModel.fromJson(e)).toList() 
+          : [];
+  }
+
+  Future<int> updateTask(TaskModel task) async{
+    final db = await database;
+    final res = await db.update('task', task.toJson(),where: "id = ?",whereArgs: [task.id]);
+    return res;
+  }
+
+  Future<int> deleteTaskByIdNote(int idNote)async{
+    final db = await database;
+    final res = await db.delete('task',where: "id_note = ? ",whereArgs: [idNote]);
+    return res;
+  }
 }
